678abc7358a5310993b45856c2f1df06c1873803
sgorski00 <sebastiangorski00@gmail.com>
2026-02-18 11:32:26 +0100

    chore: rename modules and upgrade to java 25/spring 4

---
+++ web-api/src/main/java/pl/sgorski/nethelt/webapi/NetheltWebBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.sgorski.nethelt.webapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetheltWebBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetheltWebBackendApplication.class, args);
+	}
+
+}
---
+++ web-api/src/test/java/pl/sgorski/nethelt/webapi/NetheltWebBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sgorski.nethelt.webapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetheltWebBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
