5981039965af34150afdb7957dbe2d6c12721201
sgorski00 <sebastiangorski00@gmail.com>
2026-02-21 11:29:52 +0100

    feat: add analysis reports and charts, update README

--- ai-model/src/ai_model/model.py
+++ ai-model/src/ai_model/model.py
@@ -1,11 +1,10 @@
 from sklearn.ensemble import IsolationForest
 
-N_ESTIMATORS = 200 # liczba drzew losowych - im więcej tym lepsze wyniki - kosztem wydajności
 RANDOM_STATE = 42
 
-def create_model(contamination : float= 0.02):
+def create_model(contamination: float, n_estimators: int) -> IsolationForest:
     return IsolationForest(
-        n_estimators=N_ESTIMATORS,
+        n_estimators=n_estimators,
         contamination=contamination,
         random_state=RANDOM_STATE,
     )
