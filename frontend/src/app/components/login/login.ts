b8a98c0c140a6741869ace584a898c495abec13c
Sebastian Górski <sebastiangorski00@gmail.com>
2026-06-28 12:56:51 +0200

    NET-4: init form, configure httpclient and authservice

--- frontend/src/app/components/login/login.ts
+++ frontend/src/app/components/login/login.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../services/auth-service';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { LoginRequest } from '../../models/auth/login-request';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export class Login {}
+export class Login {
+
+  private readonly authService = inject(AuthService);
+  private readonly fb = inject(FormBuilder);
+
+  public readonly loginForm = this.fb.nonNullable.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]]
+  })
+
+  public login() {
+    if(this.loginForm.invalid) {
+      return;
+    }
+
+    const request: LoginRequest = this.loginForm.getRawValue();
+
+    this.authService.login(request).subscribe({
+        next: res => console.log(res),
+        error: err => console.log(err)
+    });
+  }
+}
---
+++ frontend/src/app/services/auth-service.ts
@@ -0,0 +1,20 @@
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { LoginRequest } from '../models/auth/login-request';
+import { LoginRespone } from '../models/auth/login-response';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  
+  private readonly apiUrl = environment.apiUrl;
+  private readonly httpClient = inject(HttpClient);
+
+  public login(body: LoginRequest): Observable<LoginRespone> {
+    return this.httpClient.post<LoginRespone>(`${this.apiUrl}/auth/login`, body);
+  }
+
+}
