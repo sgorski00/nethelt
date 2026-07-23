f8d296deab27a992b51e89604d02b5722dc3d6bf
sgorski00 <sebastiangorski00@gmail.com>
2026-07-23 14:35:31 +0200

    feat: move nav, add console mock

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,29 +1,15 @@
-import { Component, inject, signal } from '@angular/core';
-import { Router, RouterLink, RouterOutlet } from '@angular/router';
-import { AuthService } from './services/auth-service';
-import { NotificationDropdown } from './components/notifications/notification-dropdown/notification-dropdown';
-import { NetworkSelector } from './components/networks/network-selector/network-selector';
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Navigation } from './components/navigation/navigation';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, NotificationDropdown, NetworkSelector],
+  imports: [RouterOutlet, Navigation],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
-  private readonly authService = inject(AuthService);
-  private readonly router = inject(Router);
   protected readonly title = signal('nethelt-frontend');
 
   public readonly currentYear = new Date().getFullYear();
-
-  public logout() {
-    this.authService.logout().subscribe({
-      next: () => this.router.navigateByUrl('/login'),
-    });
-  }
-
-  public isAuthenticated() {
-    return this.authService.isAuthenticated();
-  }
 }
---
+++ frontend/src/app/components/navigation/navigation.ts
@@ -0,0 +1,29 @@
+import { Component, computed, inject } from '@angular/core';
+import { NetworkSelector } from '../networks/network-selector/network-selector';
+import { NotificationDropdown } from '../notifications/notification-dropdown/notification-dropdown';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth-service';
+import { NetworkContextService } from '../../services/network-context-service';
+
+@Component({
+  selector: 'app-navigation',
+  imports: [NetworkSelector, NotificationDropdown, RouterLink],
+  templateUrl: './navigation.html',
+  styleUrl: './navigation.scss',
+})
+export class Navigation {
+  private readonly authService = inject(AuthService);
+  private readonly networkContextService = inject(NetworkContextService);
+  private readonly router = inject(Router);
+
+  protected readonly isAuthenticated = this.authService.isAuthenticated;
+  protected readonly isActiveNetworkSelected = computed(
+    () => this.networkContextService.activeNetwork() !== null,
+  );
+
+  public logout() {
+    this.authService.logout().subscribe({
+      next: () => this.router.navigateByUrl('/login'),
+    });
+  }
+}
